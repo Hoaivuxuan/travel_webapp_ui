d4740cb156b8577a478e68b50e6b09353d7b30ae
Vu Xuan Hoai <86643318+Hoaivuxuan@users.noreply.github.com>
2024-10-14 21:12:54 +0700

    update format code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Booking.com',
+  title: "Booking.com",
   description:
-    'A booking.com clone built with Next.js and TailwindCSS to book flights and hotels',
+    "A booking.com clone built with Next.js and TailwindCSS to book flights and hotels",
 };
 
 export default function RootLayout({
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
         <Header />
         {children}
